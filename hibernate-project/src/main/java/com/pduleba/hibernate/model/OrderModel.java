716840423d922705c272dabf99275f04d5dda755
pduleba <pduleba@pgs-soft.com>
2015-12-17 15:27:26 +0100

    Changes

--- hibernate-project/src/main/java/com/pduleba/hibernate/model/OrderModel.java
+++ hibernate-project/src/main/java/com/pduleba/hibernate/model/OrderModel.java
@@ -13,10 +13,12 @@ import javax.persistence.SequenceGenerator;
 import javax.persistence.Table;
 
 import lombok.Data;
+import lombok.EqualsAndHashCode;
 
 @Entity
 @Table(name="T_ORDERS")
 @SequenceGenerator(name="orders-sequence-generator", sequenceName="ORDERS_SEQ", initialValue=0, allocationSize=1)
+@EqualsAndHashCode(exclude="owner")
 public @Data class OrderModel {
 
 	@Id
@@ -30,37 +32,5 @@ public @Data class OrderModel {
 	@ManyToOne(cascade=CascadeType.ALL, fetch=FetchType.EAGER)
 	@JoinColumn(name="id_user")
 	private UserModel owner;
-
-	@Override
-	public boolean equals(Object obj) {
-		if (this == obj)
-			return true;
-		if (obj == null)
-			return false;
-		if (getClass() != obj.getClass())
-			return false;
-		OrderModel other = (OrderModel) obj;
-		if (id == null) {
-			if (other.id != null)
-				return false;
-		} else if (!id.equals(other.id))
-			return false;
-		if (orderDetails == null) {
-			if (other.orderDetails != null)
-				return false;
-		} else if (!orderDetails.equals(other.orderDetails))
-			return false;
-		return true;
-	}
-
-	@Override
-	public int hashCode() {
-		final int prime = 31;
-		int result = 1;
-		result = prime * result + ((id == null) ? 0 : id.hashCode());
-		result = prime * result + ((orderDetails == null) ? 0 : orderDetails.hashCode());
-		return result;
-	}
-	
 	
 }

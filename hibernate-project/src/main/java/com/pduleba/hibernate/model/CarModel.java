2e64cc1a41a73a7694049f10c7241018342dc2a2
pduleba <pduleba@pgs-soft.com>
2016-01-08 10:57:55 +0100

    Changes

--- hibernate-project/src/main/java/com/pduleba/hibernate/model/CarModel.java
+++ hibernate-project/src/main/java/com/pduleba/hibernate/model/CarModel.java
@@ -1,12 +1,8 @@
 package com.pduleba.hibernate.model;
 
 import javax.persistence.Column;
+import javax.persistence.EmbeddedId;
 import javax.persistence.Entity;
-import javax.persistence.GeneratedValue;
-import javax.persistence.GenerationType;
-import javax.persistence.Id;
-import javax.persistence.IdClass;
-import javax.persistence.SequenceGenerator;
 import javax.persistence.Table;
 
 import lombok.Data;
@@ -14,25 +10,17 @@ import lombok.NoArgsConstructor;
 
 @Entity
 @Table(name = "T_CAR")
-@IdClass(value = CarPK.class) // <- Composite Key Declaration
 @NoArgsConstructor
 public @Data class CarModel {
 
 	public CarModel(String name, String dateId) {
 		super();
-		this.name = name;
+		this.id = new CarPK(name);
 		this.dateId = dateId;
 	}
 
-	@Id
-	@Column(name = "ID")
-	@GeneratedValue(generator="car-id-generator",  strategy = GenerationType.SEQUENCE)
-	@SequenceGenerator(name = "car-id-generator", sequenceName="CAR_SEQ", initialValue=1, allocationSize=1)
-	private Long id;
-
-	@Id
-	@Column(name = "NAME")
-	private String name;
+	@EmbeddedId
+	private CarPK id;
 
 	@Column(name = "DATE_ID")
 	private String dateId;
--- hibernate-project/src/main/java/com/pduleba/hibernate/model/CarPK.java
+++ hibernate-project/src/main/java/com/pduleba/hibernate/model/CarPK.java
@@ -2,13 +2,29 @@ package com.pduleba.hibernate.model;
 
 import java.io.Serializable;
 
-import lombok.Data;
+import javax.persistence.Column;
+import javax.persistence.Embeddable;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
 
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Embeddable
+@NoArgsConstructor
 public @Data class CarPK implements Serializable {
 
-	private static final long serialVersionUID = -8942935027275183098L;
+	private static final long serialVersionUID = 2395097171032092217L;
 
+	public CarPK(String name) {
+		super();
+		this.name = name;
+	}
+
+	@Column(name = "ID")
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	private Long id;
-	
+
+	@Column(name = "NAME")
 	private String name;
 }

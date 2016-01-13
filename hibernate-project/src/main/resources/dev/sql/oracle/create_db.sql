09d0395f85b75d0ae2e5e48e2810ab2639b02927
pduleba <pduleba@pgs-soft.com>
2016-01-13 10:52:14 +0100

    Changes

--- hibernate-project/src/main/resources/dev/sql/oracle/create_db.sql
+++ hibernate-project/src/main/resources/dev/sql/oracle/create_db.sql
@@ -5,6 +5,7 @@ CREATE TABLE HIBERNATE.T_CAR (
   ID                      	NUMBER(19),
   NAME             			VARCHAR2(100 CHAR),
   DATE_ID             		VARCHAR2(100 CHAR),
+  ACTIVE             		VARCHAR2(100 CHAR),
   CONSTRAINT PK_CAR PRIMARY KEY (ID)
 );
 
